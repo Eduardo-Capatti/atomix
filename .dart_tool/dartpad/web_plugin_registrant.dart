4d690a56b13371ca5f64114a1f7b564ce944f1e7
Eduardo Capatti <ercapatti@gmail.com>
2026-05-15 12:47:10 -0300

    Fix: atualizar as dependências do aplicativo

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,15 +6,13 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:package_info_plus/src/package_info_plus_web.dart';
-import 'package:video_player_web/video_player_web.dart';
-import 'package:wakelock_plus/src/wakelock_plus_web_plugin.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:youtube_player_iframe_web/src/web_youtube_player_iframe_platform.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  PackageInfoPlusWebPlugin.registerWith(registrar);
-  VideoPlayerPlugin.registerWith(registrar);
-  WakelockPlusWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  WebYoutubePlayerIframePlatform.registerWith(registrar);
   registrar.registerMessageHandler();
 }
